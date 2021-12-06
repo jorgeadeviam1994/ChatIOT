12ef96c9ac20a692b8684c4f785d5e9324424765
Jorge Devia <jorgeadeviam1994@gmail.com>
2021-12-06 07:08:14 -0500

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> git@github.com:jorgeadeviam1994/ChatIOT.git </p>
         <a
           className="App-link"
           href="https://reactjs.org"
